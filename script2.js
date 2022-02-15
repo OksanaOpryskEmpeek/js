0e848d6973727085d1391c23050650f8aaa2135f
OksanaOpryskEmpeek <oksana.oprysk@empeek.io>
2022-02-15 23:22:23 +0200

    initial commin

---
+++ script2.js
@@ -0,0 +1,35 @@
+// function getMin(a,b){
+//   return a < b ? a : b;
+// }
+// function getMax(a,b){
+//   return a > b ? a : b;
+// }
+
+
+// const paramA = prompt("Enter a:", '');
+// const paramB = prompt("Enter b:", '');
+// const min = getMin(paramA,paramB);
+// const max = getMax(paramA,paramB);
+// alert ("Min is = " + min);
+// alert ("Max is = " + max);
+
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+const handleYes = () =>  alert("Вы согласились.");
+const handleNo = () =>  alert("Вы отменили выполнение.");
+let ask = (question, yes, no) => confirm(question) ?
+yes():
+no();
+
+ask("uestion", handleYes, handleNo)
+
---
+++ scripts.js
@@ -0,0 +1,159 @@
+// const a= prompt("What is your name?");
+// alert(`My name is ${a}`);
+
+
+
+// let b = "78";
+// console.log(typeof(b));
+
+// b= Number(b);
+// console.log(typeof(b));
+
+
+
+// const z= true;
+// console.log(typeof z);
+
+// let x = String(z);
+// console.log(x);
+
+
+
+
+// const g= undefined;
+// console.log(typeof g);
+
+// let x= Boolean(g);
+// console.log(x);
+
+// const u= undefined;
+// console.log(typeof u);
+
+// const v = Number(u);
+// console.log(u);
+
+// let a = prompt("Первое число?", 1);
+// console.log(typeof a);
+// let b = prompt("Второе число?", 2);
+// console.log(typeof b);
+
+// const x = Number(a);
+// const y = Number(b);
+
+// alert(x + y); // 3
+
+
+
+// let cc= prompt("What's the официальное название JavaScript?", "")
+// if (cc=="ECMAScript") 
+// { alert("True")
+// }else
+// { alert ("False")
+// }
+
+
+
+// const num = prompt("Enter a number:", "");
+// if (num > 0) {
+//   alert (1);
+// } else if (num < 0) {
+//   alert (-1)
+// } else(alert(0))
+
+
+// let result= (a + b < 4) ? 'Мало' : 'Много';
+
+
+
+// let message = (login == 'Сотрудник') ? 'Привет' : 
+// (login == 'Директор') ? 'Здравствуйте': 
+// (login == '') ? 'Нет логина' : 
+// '';
+
+
+
+// let age= Number(prompt("Enter age:",""));
+// if (!(age >= 14 ) || !(age <= 90 )) {
+//   alert("You are lucky!");
+// } else {
+//   alert("You are looser!")
+// }
+
+
+// let age= Number(prompt("Enter age:",""));
+// if (age <= 14 || age >= 90 ) {
+//   alert("You are lucky!");
+// } else {
+//   alert("You are looser!")
+// }
+
+
+/*Login page verification
+let user = prompt ("Who is it?", "");
+if (user == "Admin"){
+let password = prompt("Enter Password:", "");
+   if(password == "I am a boss"){
+     alert("You are welcome!");
+   } else if (password !== "I am a boss"){
+     alert("This is a wrong password!");
+   } else ( alert("Login is canceled!"))
+     
+} else if ( user =="" || user = null) {
+alert("Cancel!");
+}else (alert("I don't know you!"))
+*/
+
+
+// for (let i = 0; i < 5; i++) alert( i );
+
+/* important
+let i = 0;
+console.log(i); // 0
+console.log(i++); // 0
+console.log(++i); // 2
+console.log(i++); // 2
+console.log(i) // 3
+*/
+
+// for (let i = 2; i <=10; i++) 
+// {
+//   if (i % 2 !== 0) continue;
+//   alert( i );
+// } 
+
+
+// let browser = prompt('Імя браузера:');
+//  if (browser == 'Edge'){
+//     alert( "You've got the Edge!" );
+//  } else if ( browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera'){
+//     alert( 'Okay we support these browsers too' );
+//  } else{
+//       alert( 'We hope that this page looks ok!' );
+//  }
+    
+    
+
+
+// function checkAge(age){
+//   return (age > 18) ?  true : confirm("Родители разрешили?");
+// };
+
+
+// function checkAge(age){
+//   return (age > 18) || confirm("Родители разрешили?")
+// }
+
+// function minNumber(a,b){
+
+
+// }
+
+
+// function pow(x,n){
+//   return Math.pow(x, n);
+// }
+// const x= prompt ("Enter x", "")
+// const n = prompt("Enter n", "")
+// const powResult = pow(x,n);
+//   alert (powResult);
+
